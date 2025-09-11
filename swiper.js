c98165124790ba4f165da2f713053d42b6e06b2c
FadyNader0 <fadyn940@gmail.com>
2025-09-11 04:12:51 +0300

    test for solve problem on links in mobile

--- swiper.js
+++ swiper.js
@@ -1,15 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
   effect: "cards",
+  grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-//    loop: true,
-  coverflowEffect: {
-    rotate: 30,
-    stretch: 0,
-    depth: 200,
-    modifier: 1,
-    slideShadows: true,
-  },
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -18,6 +12,9 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
   preventClicks: false,
   preventClicksPropagation: false,
+  simulateTouch: true,
+  touchStartPreventDefault: false,
 });
