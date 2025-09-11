3dc41a7d916076d78645d5751ed2eab119f1db64
FadyNader0 <fadyn940@gmail.com>
2025-09-11 03:38:32 +0300

    add swiper and add 3 projects

---
+++ swiper.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  centeredSlides: true,
+  slidesPerView: "auto",
+//    loop: true,
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
